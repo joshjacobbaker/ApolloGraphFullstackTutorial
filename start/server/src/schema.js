92cd0730d539ef80f2d3eccde710cf8535256bd1
Josh Baker <josh.jacob.baker@gmail.com>
2021-04-27 15:41:49 -0700

    Finished 3rd step in tutorial... Write query resolvers

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -31,12 +31,6 @@ const typeDefs = gql`
     LARGE
   }
 
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    me: User
-  }
-
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
@@ -47,6 +41,27 @@ const typeDefs = gql`
     success: Boolean!
     message: String
     launches: [Launch]
+  }
+
+  type Query {
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 `
 
